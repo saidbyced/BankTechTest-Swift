732139465443fae3b502782c02d723ea9ed57a04
Chris Eadie <chris.eadie@outlook.com>
2020-08-31 14:56:48 +0100

    Add buttons to allow for deposit and withdrawal

--- BankTechTest-Swift/ContentView.swift
+++ BankTechTest-Swift/ContentView.swift
@@ -9,10 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    var account = Account()
+    
     var body: some View {
-        Text(
-            "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00"
-        )
+        VStack {
+            HStack {
+                HStack {
+                    Button(
+                        action: { print("deposit made") },
+                        label: { Text("Deposit (+)") }
+                    )
+                        .padding(5)
+                }
+                Button(
+                    action: { print("withdrawl made") },
+                    label: { Text("Withdraw (-)") }
+                )
+                    .padding(5)
+            }
+            Text(
+                "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00"
+            )
+        }
     }
 }
 
