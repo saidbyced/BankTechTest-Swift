1689d2adb0b49efa7eee0814e910dfbb5633eeb5
Chris Eadie <chris.eadie@outlook.com>
2020-08-31 14:38:12 +0100

    Ensure statement can be displayed using output from example

--- BankTechTest-Swift/ContentView.swift
+++ BankTechTest-Swift/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text(
+            "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00"
+        )
     }
 }
 
