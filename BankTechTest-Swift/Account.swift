ee275c483956bafe5b2133d948e0f148688fcc19
Chris Eadie <chris.eadie@outlook.com>
2020-09-07 11:21:54 +0100

    Add computed variable statement to account

--- BankTechTest-Swift/Account.swift
+++ BankTechTest-Swift/Account.swift
@@ -29,4 +29,33 @@ class Account {
     func withdraw(_ amount: Float) {
         transactions.append(Transaction(date: Date(), amount: amount, type: .withdrawal))
     }
+    
+    var statement: [String] {
+        var balance = Float(0)
+        var statement: [String] = []
+        
+        for transaction in transactions {
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "en_GB")
+            dateFormatter.setLocalizedDateFormatFromTemplate("yyyyMMdd")
+            let transactionDate = dateFormatter.string(from: transaction.date)
+            let transactionAmount = String(format: "%.2f", transaction.amount)
+            
+            switch transaction.type {
+            case .deposit:
+                balance += transaction.amount
+                let balanceAmount = String(format: "%.2f", balance)
+                statement.append("\(transactionDate) || \(transactionAmount) || || \(balanceAmount)")
+            case .withdrawal:
+                balance -= transaction.amount
+                let balanceAmount = String(format: "%.2f", balance)
+                statement.append("\(transactionDate) || || \(transactionAmount) || \(balanceAmount)")
+            default:
+                let balanceAmount = String(format: "%.2f", balance)
+                statement.append("\(transactionDate) || || || \(balanceAmount)")
+            }
+        }
+        
+        return statement
+    }
 }
